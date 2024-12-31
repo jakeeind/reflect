f28b99ef412c42d761ef8988512b4ae3593ef5e8
jakeeind <jakee_ind@hotmail.com>
2024-12-31 18:56:31 +0700

    feat: 🎸 protected route

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,21 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import * as auth from '$lib/server/auth.js';
+
+function isProtectedRoute(url: string | null) {
+	if (!url) return false;
+	return url.split('/').includes('(auth)');
+}
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
+
 	if (!sessionToken) {
 		event.locals.user = null;
 		event.locals.session = null;
+		if (isProtectedRoute(event.route.id)) {
+			redirect(302, '/login');
+		}
+
 		return resolve(event);
 	}
 
